b586f80d4e949909712987582719694651b6d5b9
koos <koos@terraclearinc.com>
2018-11-16 00:49:59 -0800

    domain transform bug fix..

--- basicpid.cpp
+++ basicpid.cpp
@@ -15,13 +15,16 @@ namespace terraclear
     double basicpid::domain_transform(double from_value, double from_min, double from_max, double to_min,  double to_max)
     {
         //constrain input value to fit between from_max and from_min
-        double from_constrained = std::min(std::max(from_min, from_value), from_max);;
-
+        double from_constrained = std::min(std::max(from_min, from_value), from_max);
+        
         //normalize input and output values.
         double from_scale = from_max - from_min;
         double to_scale = to_max - to_min;
+        
+        //rebase the constrained value and prevent a divide by zero if from_value was constrained to from_min
         double from_rebased =  (from_constrained <= from_min) ? from_constrained : from_constrained - from_min;
 
+        //do the [from -> to] domain transform..
         double retval = ((from_rebased / from_scale) * to_scale) + to_min;
 
         return retval;
